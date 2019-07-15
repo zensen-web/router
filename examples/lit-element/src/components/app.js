d2e098b3b2c621e3f94035c539839d673fb5c937
Travis J True <travis@truesoftent.com>
2019-07-15 00:32:46 -0700

    Added a routing example for lit-element

--- examples/lit-element/src/components/app.js
+++ examples/lit-element/src/components/app.js
@@ -1,3 +1,5 @@
+import { matchRouteSwitch, EVENT_ROUTE_CHANGE } from '@travistrue2008/zen-router'
+
 import {
   LitElement,
   html,
@@ -5,6 +7,12 @@ import {
 } from 'lit-element'
 
 class App extends LitElement {
+  static get properties () {
+    return {
+      __route: String,
+    }
+  }
+
   static get styles () {
     return css`
       *,
@@ -22,13 +30,98 @@ class App extends LitElement {
         display: flex;
         width: 100%;
         height: 100%;
+        flex-flow: nowrap column;
+      }
+
+      .nav {
+        display: flex;
+        padding-left: 1.2rem;
+        width: 100%;
+        height: 6rem;
+        align-items: center;
+        background-color: #353535;
+      }
+
+      .nav-item {
+        margin-right: 1.2rem;
+        color: #FFF;
+        font-size: 1.4rem;
+        text-decoration: none;
+      }
+
+      .spacer {
+        height: 2rem;
       }
     `
+  }
+
+  constructor () {
+    super()
+    this.__initState()
+    this.__initHandlers()
+  }
+
+  __initState () {
+    this.__route = ''
+    this.__navItems = this.genNavItems()
+  }
+
+  __initHandlers () {
+    this.__handlers = {
+      changeRoute: e => (this.__route = e.detail),
+    }
+  }
+
+  connectedCallback () {
+    super.connectedCallback()
+
+    window.addEventListener(EVENT_ROUTE_CHANGE, this.__handlers.changeRoute)
+  }
+
+  disconnectedCallback () {
+    super.disconnectedCallback()
+
+    window.removeEventListener(EVENT_ROUTE_CHANGE, this.__handlers.changeRoute)
+  }
+
+  genNavItems () {
+    return [
+      {
+        exact: false,
+        label: 'Home',
+        path: '/home/',
+        href: '#/home',
+        renderer: () => html`
+          <p>Home</p>
+        `,
+      },
+      {
+        exact: false,
+        label: 'About Us',
+        path: '/about/',
+        href: '#/about',
+        renderer: () => html`
+          <p>About Us</p>
+        `,
+      },
+    ]
   }
 
   render () {
     return html`
       <div class="container">
+        <nav class="nav">
+          ${this.__navItems.map(item => html`
+            <a
+              class="nav-item"
+              href="${item.href}"
+            >${item.label}</a>
+          `)}
+        </nav>
+
+        <div class="spacer"></div>
+
+        ${matchRouteSwitch(this.__navItems, this.__route)}
       </div>
     `
   }
