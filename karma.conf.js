9238cca84b19e8405d6e55e2230a9d491aee0165
travistrue2008 <travis.true08@gmail.com>
2019-07-25 01:18:31 -0700

    Updated karma config to use puppeteer.

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,8 @@ const path = require('path')
 const merge = require('webpack-merge')
 const createDefaultConfig = require('@open-wc/testing-karma/default-config')
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = config => {
   config.set(
     merge(createDefaultConfig(config), {
@@ -9,10 +11,25 @@ module.exports = config => {
       concurrency: Infinity,
       logLevel: config.LOG_INFO,
       frameworks: ['mocha', 'chai'],
-      browsers: ['ChromeHeadless'],
+      browsers: ['HeadlessChromium'],
       files: [
         config.grep ? config.grep : 'test/**/*.test.js',
       ],
+      customLaunchers: {
+        HeadlessChromium: {
+          base: 'ChromeHeadless',
+          flags: [
+            '--v=1',
+            '--no-sandbox',
+            '--remote-debugging-port=9222',
+            '--enable-logging',
+            '--disable-background-timer-throttling',
+            '--disable-renderer-backgrounding',
+            '--proxy-bypass-list=*',
+            '--proxy-server=\'direct://\'',
+          ],
+        },
+      },
       webpack: {
         mode: 'development',
         devtool: 'inline-source-map',
