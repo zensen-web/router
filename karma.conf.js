f1af54d3ebb545d951d94bf9a7e157b90e2476b7
travistrue2008 <travis.true08@gmail.com>
2020-06-08 01:09:14 -0700

    Modernized

--- karma.conf.js
+++ karma.conf.js
@@ -1,54 +1,14 @@
-const path = require('path')
-const merge = require('webpack-merge')
 const createDefaultConfig = require('@open-wc/testing-karma/default-config')
-
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+const merge = require('deepmerge')
 
 module.exports = config => {
   config.set(
     merge(createDefaultConfig(config), {
-      colors: true,
-      concurrency: Infinity,
-      logLevel: config.LOG_INFO,
-      frameworks: ['mocha', 'chai'],
-      browsers: ['HeadlessChromium'],
       files: [
-        config.grep ? config.grep : 'test/**/*.test.js',
+        { pattern: config.grep ? config.grep : 'test/**/*.test.js', type: 'module' },
       ],
-      customLaunchers: {
-        HeadlessChromium: {
-          base: 'ChromeHeadless',
-          flags: [
-            '--v=1',
-            '--no-sandbox',
-            '--remote-debugging-port=9222',
-            '--enable-logging',
-            '--disable-background-timer-throttling',
-            '--disable-renderer-backgrounding',
-            '--proxy-bypass-list=*',
-            '--proxy-server=\'direct://\'',
-          ],
-        },
-      },
-      webpack: {
-        mode: 'development',
-        devtool: 'inline-source-map',
-        module: {
-          rules: [
-            {
-              test: /\.js/,
-              include: path.resolve(__dirname),
-              exclude: /(test|node_modules)/,
-              enforce: 'post',
-              use: {
-                loader: 'istanbul-instrumenter-loader',
-                options: {
-                  esModules: true,
-                },
-              },
-            },
-          ],
-        },
+      esm: {
+        nodeResolve: true,
       },
     }),
   )
